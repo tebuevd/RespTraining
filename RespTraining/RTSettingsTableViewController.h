91ef8a7d77513314d2ceb519721b78a884a0b564
Dinislam Tebuev <tebuevd@stanford.edu>
2014-08-12 11:22:42 -0700

    Settings view controller + view added

---
+++ RespTraining/RTSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RTSettingsTableViewController.h
+//  RespTraining
+//
+//  Created by Dinislam Tebuev on 8/12/14.
+//  Copyright (c) 2014 Dinislam Tebuev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTSettingsTableViewController : UITableViewController
+
+@end
